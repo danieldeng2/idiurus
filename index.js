d406e2d45b65c73ea7685e3d760e4e0cbbf90230
danieldeng2 <danieldeng223@gmail.com>
2022-02-05 11:51:48 +0000

    implement basic mouse movement

---
+++ index.js
@@ -0,0 +1,12 @@
+import robotjs from 'robotjs';
+const { getMousePos, moveMouse, mouseClick } = robotjs;
+
+//Get the mouse position, retuns an object with x and y. 
+var mouse=getMousePos();
+console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
+
+//Move the mouse down by 100 pixels.
+moveMouse(mouse.x,mouse.y+100);
+
+//Left click!
+mouseClick();
